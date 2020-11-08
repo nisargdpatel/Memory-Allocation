2834936ec82e675a1cfbfbec7aae1b94bcd4c9d6
Geoff <gjlynch@umich.edu>
2020-11-08 16:46:50 -0500

    initial algorithm complete

--- CIS450Program2.py
+++ CIS450Program2.py
@@ -1,4 +1,5 @@
-import random
+import random, math
+from itertools import cycle
 
 class Job():
     # Constructor with required fields
@@ -58,60 +59,208 @@ class Job():
             elements.append(HeapElement())
         return elements
 
-
 class HeapElement():
     # Constructor with required fields
     def __init__(self):
         self.memory = self.generate_memory()
+        self.ffLocation = ''
+        self.nfLocation = ''
+        self.bfLocation = ''
+        self.wfLocation = ''
+        self.lifeTime = 0
 
     # Generates memory size of each heap element
     def generate_memory(self):
         return 35 + ( random.randint(0, 15) * random.choice([-1, 1]) )
 
+def printHeap(tempJo, tempOutFile):
+    counter = 1
+    print("Run Time: ", tempJo.running_time)
+    tempOutFile.write("\nRun Time: " + str(tempJo.running_time) + "\n")
+    print("Code Size: ", tempJo.code_size)
+    tempOutFile.write("Code Size: " + str(tempJo.code_size) + "\n")
+    print("Stack Size: ", tempJo.stack_size)
+    tempOutFile.write("Stack Size: " + str(tempJo.stack_size) + "\n")
+
+    if (tempJo.size.lower() == 'small'):
+        multiplier = 50
+    elif (tempJo.size.lower() == 'medium'):
+        multiplier = 100
+    elif (tempJo.size.lower() == 'large'):
+        multiplier = 250
+
+    print("Heap Elements: ", tempJo.running_time, " * ", multiplier, " = ", tempJo.running_time*multiplier, " heap elements")
+    tempOutFile.write("Heap Elements: " + str(tempJo.running_time) + " * " + str(multiplier) + " = " + str(tempJo.running_time*multiplier) + " heap elements\n")
+    print(multiplier, " heap elements arrive each time unit\n")
+    tempOutFile.write(str(multiplier) + " heap elements arrive each time unit\n")
+
+    for element in tempJo.heap_elements:
+
+        if(element.memory < 20 or element.memory > 50):
+            print("Heap element ", counter, ": ", element.memory, " memory\t->\tNot in the range 35+/-15")
+            tempOutFile.write("Heap element " + str(counter) + ": " + str(element.memory) + " memory\t->\tNot in the range 35+/-15\n")
+        else:
+            print("Heap element ", counter, ": ", element.memory, " memory")
+            tempOutFile.write("Heap element " + str(counter) + ": " + str(element.memory) + " memory\n")
+        counter = counter+1
+
+def runSimulation(testName, memoryUnitSize, memoryNumber, outputFile, logFile, lostObjects, smallJobs, mediumJobs, largeJobs):
+    jobPercents = []
+    jobPercents.extend('small' for x in range(smallJobs))
+    jobPercents.extend('medium' for x in range(mediumJobs))
+    jobPercents.extend('large' for x in range(largeJobs))
+    jobs = []
+    nextJob = 3 + ( random.choice([1, 2]) * random.choice([-1, 1]) )
+    memory = []
+    jobTypes = {
+                "small":0,
+                "medium":0,
+                "large":0,
+            }
+
+    alg = Algorithms()
+    jobTime = 0
+
+    for currentTime in range(1,12001):
+        print(currentTime)
+        # Add a job and generate the next arrival time
+        if currentTime == nextJob:
+            # add job to job queue
+            jobs.append(Job(random.choice(jobPercents)))
+
+            # determine next arrival time
+            nextJob = currentTime + (3 + ( random.choice([1, 2]) * random.choice([-1, 1]) ))
+
+        # Clear out a job that has finished its run time
+        if jobTime == 0 and memory:
+            memory.pop()
+
+        # Add jobs to the memory unit if it is available
+        if not memory and jobs:
+            # Add job to the memory units if it can fit the code and stack size
+            if (memoryUnitSize*memoryNumber) > (jobs[0].code_size + jobs[0].stack_size):
+                jobTypes[jobs[0].size] = jobTypes[jobs[0].size] + 1
+                memory.append(jobs.pop())
+                jobTime = memory[0].running_time
+                heapsPerUnit = math.ceil(len(memory[0].heap_elements) / jobTime)
+                heapCounter = 0
+                heap = [memoryUnitSize] * (memoryNumber - (math.ceil(memory[0].code_size / memoryUnitSize) + math.ceil(memory[0].stack_size / memoryUnitSize)))
+                alg.ffHeap = heap
+                alg.nfHeap = heap
+                alg.bfHeap = heap
+                alg.wfHeap = heap
+                alg.nfPrevious = 0
+            else:
+                # Job cannot fit in the memory units so move it to the end of the queue
+                jobs.append(jobs.pop())
+
+        # If there is a job in memory, begin allocation/deallocation
+        if memory:
+            # Heap elements are allocated evenly across run time
+            for number in range(heapsPerUnit):
+                # Attempt to allocate each heap element via the 4 algorithms
+                heapElement = memory[0].heap_elements[heapCounter]
+                heapElement.ffLocation = alg.mallocFF(heapElement.memory)
+                heapElement.nfLocation = alg.mallocNF(heapElement.memory)
+                heapElement.bfLocation = alg.mallocBF(heapElement.memory)
+                heapElement.wfLocation = alg.mallocWF(heapElement.memory)
+                heapElement.lifeTime = random.randint(1, memory[0].jobTime)
+                heapCounter += 1
+
+            # reduce the lifeTime as it has consumed 1 time unit
+            for element in memory[0].heap_elements:
+                element.lifeTime -= 1
+                # if lifetime has completed, free up allocated memory
+                if element.lifeTime == 0:
+                    # if 100th job type skip memory freeing to simulate lost objects
+                    if lostObjects and (jobTypes[memory[0].size] % 100) == 0:
+                        continue
+                    else:
+                        freeFF(element.ffLocation, memoryUnitSize)
+                        freeNF(element.nfLocation, memoryUnitSize)
+                        freeBF(element.bfLocation, memoryUnitSize)
+                        freeWF(element.wfLocation, memoryUnitSize)
+
+        # Counter for the current running job
+        jobTime -= 1
+
+        if ((currentTime % 20) == 0) and currentTime > 2000:
+            # PRINT METRICS FOR EVERY 20 TIME UNITS
+            pass
 
 
+        if currentTime == 2000:
+            # PRINT PREFIL STEADY STATE METRICS
+            pass
 
+    #UPDATE SUMMARY FILE once the full simulation is complete
+
+
+class Algorithms:
+    def __init__(self):
+        self.ffHeap = []
+        self.nfHeap = []
+        self.nfPrevious = 0
+        self.bfHeap = []
+        self.wfHeap = []
+
+    def mallocFF(self, size):
+        r = ''
+        for index, x in enumerate(self.ffHeap):
+            if x >= size:
+                self.ffHeap[index] -= size
+                r = index
+                break
+        return r
+
+    def mallocNF(self, size):
+        r = ''
+        index = self.nfPrevious
+        nfHeapE = self.nfHeap[0:self.nfPrevious]
+        del self.nfHeap[0:self.nfPrevious]
+        self.nfHeap.extend(nfHeapE)
+        for index, x in enumerate(cycle(self.nfHeap)):
+            if index >= len(self.nfHeap)-1:
+                index = 0
+            if x >= size:
+                self.nfHeap[index] -= size
+                self.nfPrevious = index
+                r = index
+                break
+        return r
+
+    def mallocBF(self, size):
+        r = ''
+        for x in sorted(self.bfHeap):
+            if x >= size:
+                r = self.bfHeap.index(x)
+                self.bfHeap[self.bfHeap.index(x)] -= size
+                break
+        return r
+
+    def mallocWF(self, size):
+        r = ''
+        for x in sorted(self.wfHeap, reverse=True):
+            if x >= size:
+                r = self.wfHeap.index(x)
+                self.wfHeap[self.wfHeap.index(x)] -= size
+                break
+        return r
+
+    def freeFF(self, location, memoryUnitSize):
+        ffHeap[location] = memoryUnitSize
+
+    def freeNF(self, location, memoryUnitSize):
+        nfHeap[location] = memoryUnitSize
+
+    def freeBF(self, location, memoryUnitSize):
+        bfHeap[location] = memoryUnitSize
+
+    def freeWF(self, location, memoryUnitSize):
+        wfHeap[location] = memoryUnitSize
 
 def main():
-
-    
-    
-
-    def printHeap(tempJo, tempOutFile):
-        counter = 1
-        print("Run Time: ", tempJo.running_time)
-        tempOutFile.write("\nRun Time: " + str(tempJo.running_time) + "\n")
-        print("Code Size: ", tempJo.code_size)
-        tempOutFile.write("Code Size: " + str(tempJo.code_size) + "\n")
-        print("Stack Size: ", tempJo.stack_size)
-        tempOutFile.write("Stack Size: " + str(tempJo.stack_size) + "\n")
-
-        if (tempJo.size.lower() == 'small'):
-            multiplier = 50
-        elif (tempJo.size.lower() == 'medium'):
-            multiplier = 100
-        elif (tempJo.size.lower() == 'large'):
-            multiplier = 250
-
-        print("Heap Elements: ", tempJo.running_time, " * ", multiplier, " = ", tempJo.running_time*multiplier, " heap elements")
-        tempOutFile.write("Heap Elements: " + str(tempJo.running_time) + " * " + str(multiplier) + " = " + str(tempJo.running_time*multiplier) + " heap elements\n")
-        print(multiplier, " heap elements arrive each time unit\n")
-        tempOutFile.write(str(multiplier) + " heap elements arrive each time unit\n")
-
-        for element in tempJo.heap_elements:
-            
-            if(element.memory < 20 or element.memory > 50):
-                print("Heap element ", counter, ": ", element.memory, " memory\t->\tNot in the range 35+/-15")
-                tempOutFile.write("Heap element " + str(counter) + ": " + str(element.memory) + " memory\t->\tNot in the range 35+/-15\n")
-            else:
-                print("Heap element ", counter, ": ", element.memory, " memory")
-                tempOutFile.write("Heap element " + str(counter) + ": " + str(element.memory) + " memory\n")
-            counter = counter+1
-
-        
-
-
-    jobSmall = Job("small")
+    """jobSmall = Job("small")
     outFileSmall = open('small_jobs_output.txt', 'w')
     printHeap(jobSmall, outFileSmall)
     outFileSmall.close()
@@ -124,15 +273,16 @@ def main():
     jobLarge = Job("large")
     outFileLarge = open('large_jobs_output.txt', 'w')
     printHeap(jobLarge, outFileLarge)
-    outFileLarge.close()
+    outFileLarge.close()"""
+
+
+    runSimulation(testName='TestRun', memoryUnitSize=8, memoryNumber=15, outputFile='', logFile='', lostObjects=False, smallJobs=50, mediumJobs=25,largeJobs=25)
+
 
     # memory_size = input("Please enter the memory unit size: ")
     # memory_units = input("Please enter the number of memory units available: ")
 
-    # determining next arrival time of a job
-    # arrival_time = 3 + ( random.randint(0, 2) * random.choice([-1, 1]) )
 
-    
 
 
 if __name__ == "__main__":
